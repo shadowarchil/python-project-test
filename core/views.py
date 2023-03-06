0c94b7b919c801fa775fcb76233f8bba6e1a8519
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2023-03-06 20:38:26 +0400

    Add home view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def home_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'home.html')
