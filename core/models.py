d4176e8feda15b806d92320d5cd2c2ed159a31fa
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2023-03-06 21:14:09 +0400

    Add question model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Question(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+    create_time = models.DateTimeField(auto_now_add=True)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-
+from core.models import Question
 
 def home_view(request: HttpRequest) -> HttpResponse:
-    return render(request, 'home.html')
+    return render(request, 'home.html', context={
+        'questions': Question.objects.all()
+    })
