fd5110d749f25e26521122ab0723f52bb753886c
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2023-03-09 20:28:55 +0400

    Add question detail page

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from core.views import HomeView
+from core.views import HomeView, QuestionDetailView
+
+app_name = 'core'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('questions/<int:pk>/', QuestionDetailView.as_view(), name='question-detail')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from core.models import Question
 
 
@@ -12,3 +12,9 @@ def home_view(request: HttpRequest) -> HttpResponse:
 class HomeView(ListView):
     template_name = 'home.html'
     queryset = Question.objects.all()
+    context_object_name = 'questions'
+
+
+class QuestionDetailView(DetailView):
+    template_name = 'question_detail.html'
+    model = Question
